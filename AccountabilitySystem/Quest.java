a46754d252cc7e36ec4cb0dc165640c0cfda8a51
Alfred-Folashade <alfredfolashade47@gmail.com>
2024-06-02 20:40:56 +0100

    Updated the Quest class by adding variables and a toString method

--- AccountabilitySystem/Quest.java
+++ AccountabilitySystem/Quest.java
@@ -1,4 +1,25 @@
 
 public abstract class Quest {
-
+	boolean isCompleted;
+	int durationOfQuest;
+	String questName;
+	String questInfo;
+	String difficultyLevel;
+	String reward;
+	
+	
+	public String toString() {
+		
+		return  "Quest: " + questName +
+				"\nStatus: " + isCompleted+ 
+				"\nDifficulty Level: " + difficultyLevel +
+				"GOALS\n" +
+				"CAUTION: If the quest remains incompleted penalties will be given accordingly. \n";
+		
+		
+	}
+	
+	
+	
+	
 }
